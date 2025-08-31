3f36ba585e270c94171ac28c16aa272eba76275a
Shreksy <davidgorelik375@gmail.com>
2025-08-31 03:41:11 +0300

    improved bonus stage activation and playability

--- idle_runner.py
+++ idle_runner.py
@@ -12,6 +12,12 @@ clicking = False
 bg = "#70a9a1"
 fg = "#f5f5f5"
 
+bonus_stage = 'bonus_stage_indicator.png'   #bonus stage star png
+bonus_stage_location = (1200,250,1400,550)
+bonus_button_l2r = 'bonus_activate_l2r.png'
+bonus_button_r2l = 'bonus_activate_r2l.png'
+bonus_active_location = (1280,1300,1400,600)
+
 win = tk.Tk()
 win.overrideredirect(True)
 win.geometry("400x100+1715+0")
@@ -27,28 +33,34 @@ def pixel_watcher():
     global clicking
     while True:
         if running:
-            bonus_stage = 'bonus_stage_indicator.png'
-            bonus_stage_location = (1200,250,1400,550)
+
             try: 
-                is_bonus_stage = pyautogui.locateOnScreen(bonus_stage, region = bonus_stage_location, confidence= 0.8)
-                if is_bonus_stage:
+                if pyautogui.locateOnScreen(bonus_stage, region = bonus_stage_location, confidence= 0.8) != None:
                     clicking = False
-                    if pyautogui.pixel(1400,1766) == (22,12,27):
-                        pyautogui.moveTo(1400,1766)
+
+                    if pyautogui.locateOnScreen(bonus_button_l2r, region = bonus_active_location, confidence= 0.5) != None:
+                        button_location = pyautogui.center(pyautogui.locateOnScreen(bonus_button_l2r, region = bonus_active_location, confidence= 0.5))
+                        pyautogui.moveTo(button_location)
+                        end_location = (button_location[0]+1150,button_location[1])
                         pyautogui.mouseDown()
-                        pyautogui.moveTo(2500,1776,duration = 1)
+                        pyautogui.moveTo(end_location,duration = 1)
                         pyautogui.mouseUp()
                         bonus_stage_player()
-                    elif pyautogui.pixel(2425,1766) == (22,12,27):
-                        pyautogui.moveTo(2425,1766)
+
+                    
+                    elif pyautogui.locateOnScreen(bonus_button_r2l, region = bonus_active_location, confidence= 0.5) != None:
+                        button_location = pyautogui.center(pyautogui.locateOnScreen(bonus_button_r2l, region = bonus_active_location, confidence= 0.5))
+                        pyautogui.moveTo(button_location)
+                        end_location = (button_location[0]-1150,button_location[1])
                         pyautogui.mouseDown()
-                        pyautogui.moveTo(1300,1776,duration = 1)
+                        pyautogui.moveTo(end_location,duration = 1)
                         pyautogui.mouseUp()
                         bonus_stage_player()
+
             except pyautogui.ImageNotFoundException:
                 pass
             
-            time.sleep(5)
+            time.sleep(0.1)
         time.sleep(0.1)
 
 
@@ -66,6 +78,8 @@ def auto_clicker():
 
 def bonus_stage_player():
     global clicking
+    time.sleep(40)
+    pyautogui.click(2150,1650)
     clicking = True
 
 
@@ -90,9 +104,9 @@ def toggle_event():
 
 
 click_thread = threading.Thread(target=auto_clicker, daemon=True)
-bonus_stage_thread = threading.Thread(target = pixel_watcher,daemon=True)
+interference_thread = threading.Thread(target = pixel_watcher,daemon=True)
 
-bonus_stage_thread.start()
+interference_thread.start()
 click_thread.start()
 
 
